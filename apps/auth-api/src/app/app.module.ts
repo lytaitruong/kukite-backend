e443378ccee34265c89ae84f51a011eb5bbad7a6
Lý Tại Trường <lytaitruong060197@gmail.com>
2023-12-19 13:58:51 +0700

    feat(setup): setup auth service and database

---
+++ apps/auth-api/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { PrismaUserModule } from '@libs/prisma-user'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+      envFilePath: ['.env'],
+      load: [],
+    }),
+    PrismaUserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
